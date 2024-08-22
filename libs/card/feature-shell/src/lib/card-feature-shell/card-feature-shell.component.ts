73a8713ade6b41cc9c01a1be70ea63f97f85da71
pablo <pnunfe@gmail.com>
2024-08-22 17:59:57 +0200

    chore: Refactor card feature shell component layout

--- libs/card/feature-shell/src/lib/card-feature-shell/card-feature-shell.component.ts
+++ libs/card/feature-shell/src/lib/card-feature-shell/card-feature-shell.component.ts
@@ -10,18 +10,11 @@ import { RouterOutlet } from '@angular/router';
     <div class="flex h-screen">
       <div class="relative flex h-full w-1/2">
         <div class="h-full w-2/3 bg-[url('../../public/images/bg-main-desktop.png')] bg-cover bg-center"></div>
-        <div class="relative z-10">
-          <div class="mb-6">
-            <img src="../../public/images/bg-card-front.png" alt="Front of Credit Card" class="w-full rounded-lg" />
-          </div>
-          <div class="p-4">
-            <img src="../../public/images/bg-card-back.png" alt="Back of Credit Card" class="w-full rounded-lg" />
-          </div>
-          <div class="h-full w-1/3"></div>
-        </div>
-        <div class="h-full w-1/2">
-          <router-outlet></router-outlet>
-        </div>
+
+        <div class="h-full w-1/3"></div>
+      </div>
+      <div class="h-full w-1/2">
+        <router-outlet></router-outlet>
       </div>
     </div>
   `,
