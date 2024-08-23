72fcccf445f232c2ffbfdd7e2905333e7a71df2a
pablo <pnunfe@gmail.com>
2024-08-23 17:39:41 +0200

    chore: Update card feature form component with improved validators and control errors

--- libs/card/feature-form/src/lib/card-feature-form/card-feature-form.component.ts
+++ libs/card/feature-form/src/lib/card-feature-form/card-feature-form.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation, inject } from '@angular/core';
 import {
@@ -18,35 +19,35 @@ import {
       <form [formGroup]="cardForm" (ngSubmit)="onsubmit()">
         <label for="cardName" class="block">CARDHOLDER NAME</label>
         <input placeholder="e.g. Jane Applessed" type="text" formControlName="cardName" required="true" />
-        @if (cardForm.get('cardName')?.hasError('required')) {
-          <div>Name is required</div>
+        @if (c.cardName.errors?.['pattern']) {
+          <div>Name can't use number</div>
         }
-        @if (cardForm.get('cardName')?.hasError('pattern')) {
-          <div>Name is invalid</div>
+        @if (c.cardName.errors?.['minlength']) {
+          <div>Name too short</div>
         }
-        @if (cardForm.get('cardName')?.hasError('minlength')) {
-          <div>Name is too short</div>
+        @if (c.cardName.touched && c.cardName.invalid) {
+          <div>Must insert name</div>
         }
 
         <label for="cardNumber" class="block">CARD NUMBER</label>
         <input placeholder="e.g. 1234 5678 9123 0000" type="text" formControlName="cardNumber" />
-        @if (cardForm.get('cardNumber')?.hasError('pattern' || ('minlength' && 'maxlength'))) {
+        @if (c.cardNumber.errors?.['pattern']) {
           <div>Card number is invalid</div>
         }
 
         <h3>EXP. DATE (MM/YY)</h3>
         <input placeholder="MM" type="text" formControlName="expDateM" minlength="2" maxlength="2" />
-        @if (cardForm.get('expDateM')?.hasError('pattern' || 'minlength' || 'maxlength' || ('max' && 'min'))) {
+        @if (c.expDateM.errors) {
           <div>Month is invalid</div>
         }
 
         <input placeholder="YY" type="text" formControlName="expDateY" minlength="2" maxlength="2" />
-        @if (cardForm.get('expDateY')?.hasError('pattern' && 'minlength' && 'maxlength' && 'yearValidator')) {
+        @if (c.expDateY.errors) {
           <div>Year is invalid</div>
         }
         <label for="cvv" class="block">CVC</label>
         <input placeholder="e.g. 123" type="text" formControlName="cvc" maxlength="3" />
-        @if (cardForm.get('cvc')?.hasError('pattern' && 'minlength' && 'maxlength')) {
+        @if (c.cvc.errors) {
           <div>CVC is invalid</div>
         }
         <div>
@@ -66,7 +67,7 @@ export class CardFeatureFormComponent {
     cardName: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(2)]],
     cardNumber: [
       '',
-      [Validators.pattern('[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}'), Validators.minLength(16), Validators.maxLength(16)],
+      [Validators.pattern(/^(\d{4}[ -]?){3}\d{4}$/), Validators.minLength(16), Validators.maxLength(19)],
     ],
     expDateM: [
       '',
@@ -86,7 +87,10 @@ export class CardFeatureFormComponent {
   });
 
   onsubmit() {
-    console.log(this.cardForm.value);
+    if (!this.cardForm.valid) {
+      console.log('Errors', this.cardForm.errors);
+    }
+    console.log('Forms Valid', this.cardForm.value);
   }
 
   yearValidator(): ValidatorFn {
@@ -94,10 +98,15 @@ export class CardFeatureFormComponent {
       const value = control.value;
       const currentYear = new Date().getFullYear();
       const year = parseInt(value, 10) + 2000;
+
       if (year < currentYear || year > currentYear + 5 || year === currentYear) {
         return { yearValidator: true };
       }
       return null;
     };
+  }
+
+  get c() {
+    return this.cardForm.controls;
   }
 }
