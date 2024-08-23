28198cf827afed7d7bed65554073d0db59e69643
pablo <pnunfe@gmail.com>
2024-08-23 18:51:24 +0200

    chore: Update YearValidator to accept yearDifference parameter

--- libs/shared/util/validators/src/lib/year-validator.ts
+++ libs/shared/util/validators/src/lib/year-validator.ts
@@ -1,12 +1,12 @@
 import { type AbstractControl, type ValidationErrors, type ValidatorFn } from '@angular/forms';
 
-export function YearValidator(): ValidatorFn {
+export function YearValidator(yearDifference: number): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const value = control.value;
     const currentYear = new Date().getFullYear();
     const year = parseInt(value, 10) + 2000;
 
-    if (year < currentYear || year > currentYear + 5 || year === currentYear) {
+    if (year < currentYear || year > currentYear + yearDifference || year === currentYear) {
       return { yearValidator: true };
     }
     return null;
